3cb1c161c5a1171049e07b393f05fcb853f9657f
ayyshaa <qacarovaayse@gmail.com>
2024-08-24 18:11:41 +0400

    work20.2

---
+++ app.js
@@ -0,0 +1,5 @@
+let name = "Elvira";
+let surname = "Eldarova";
+let fulName = name + " " + surname;
+console.log(fulName.split(" "));
+console.log(fulName.split(" ")[0][0] + " " + fulName.split(" ")[1][0]);
